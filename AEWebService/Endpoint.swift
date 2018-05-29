58a5da4a9e368470c6afaca0ccf7854b73e1cd15
Allan Evans <allan@aelyssum.com>
2018-05-29 10:45:42 -0700

    Added Endpoint protocol and extensions to Webservice for making requests with Endpoint as a parameter

---
+++ AEWebService/Endpoint.swift
@@ -0,0 +1,49 @@
+//
+//  Endpoint.swift
+//  AEWebService
+//
+//  Created by Allan Evans on 5/29/18.
+//  Copyright © 2018 AllanEvans. All rights reserved.
+//
+
+import Foundation
+
+public protocol Endpoint {
+    
+    var sessionManager: URLSession { get }
+    var url: URL { get }
+    var method: Webservice.HTTPMethod { get }
+    var queryParameters: [String: Any]? { get }
+    
+}
+
+extension Endpoint {
+    
+    var sessionManager: URLSession {
+        return URLSession.shared
+    }
+    
+}
+
+extension Webservice {
+    
+    public func request<T: Encodable>(_ endpoint: Endpoint,
+                                      body: T,
+                 catch throw: @escaping (Swift.Error)->(),
+                 complete: @escaping ()->()
+        ) {
+        guard endpoint.method != .get else {
+            `throw`(Error.wrongResponseType)
+            return
+        }
+        request(url: endpoint.url, method: endpoint.method, body: body, catch: `throw`, complete: complete)
+    }
+
+    public func get<T: Decodable>(_ endpoint: Endpoint,
+                               catch throw: @escaping (Swift.Error)->(),
+                               complete: @escaping (T)->()
+        ) {
+        get(url: endpoint.url, parameters: endpoint.queryParameters, sessionManager: endpoint.sessionManager, catch: `throw`, complete: complete)
+    }
+    
+}
--- AEWebService/Webservice.swift
+++ AEWebService/Webservice.swift
@@ -8,30 +8,27 @@
 
 import Foundation
 
-class Webservice {
+public class Webservice {
     
-    enum Error: Swift.Error {
+    public enum Error: Swift.Error {
         case unableToResolveURL
         case serverError(Int)
         case wrongResponseType
         case responseWasNil
+        case postBodyWasNil
         case deserializationError(Swift.Error)
     }
     
     var task: URLSessionTask!
     
-    var sessionManager: URLSession {
-        return URLSession.shared
-    }
-    
-    enum HTTPMethod: String {
+    public enum HTTPMethod: String {
         case get = "GET"
         case post = "POST"
         case patch = "PATCH"
         case put = "PUT"
     }
     
-    func get<T: Decodable>(url: URL, parameters: [String:Any]?, catch: @escaping (Swift.Error)->(), complete: @escaping (T)->()) {
+    public func get<T: Decodable>(url: URL, parameters: [String:Any]?, sessionManager: URLSession = URLSession.shared, catch: @escaping (Swift.Error)->(), complete: @escaping (T)->()) {
         let `throw` = `catch`
         guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
             `throw`(Error.unableToResolveURL)
@@ -50,28 +47,19 @@ class Webservice {
         }
         var request = URLRequest(url: fullyFormedUrl)
         request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         task = sessionManager.dataTask(with: request) {
             (data, response, error) in
             self.task = nil
-            guard error == nil else {
-                `throw`(error!)
-                return
-            }
-            guard let httpResponse = response as? HTTPURLResponse else {
-                `throw`(Error.wrongResponseType)
-                return
-            }
-            guard (200..<300).contains(httpResponse.statusCode) else {
-                `throw`(Error.serverError(httpResponse.statusCode))
-                return
-            }
-            guard let _data = data else {
-                `throw`(Error.responseWasNil)
+            do {
+               try self.validate(response: response, error: error)
+            } catch let error {
+                `throw`(error)
                 return
             }
             do {
-                let decoder = JSONDecoder()
-                let responseObject: T = try decoder.decode(T.self, from: _data)
+                let responseObject: T = try self.decode(data)
                 complete(responseObject)
             } catch let error {
                 `throw`(error)
@@ -80,54 +68,92 @@ class Webservice {
         task.resume()
     }
     
-    func post<T: Encodable>(url: URL, body: T, catch: @escaping (Swift.Error)->(), complete: @escaping ()->()) {
+    public func post<T: Encodable>(url: URL, body: T, sessionManager: URLSession = URLSession.shared, catch: @escaping (Swift.Error)->(), complete: @escaping ()->()) {
         request(url: url, method: .post, body: body, catch: `catch`, complete: complete)
     }
 
-    func patch<T: Encodable>(url: URL, body: T, catch: @escaping (Swift.Error)->(), complete: @escaping ()->()) {
+    public func patch<T: Encodable>(url: URL, body: T, sessionManager: URLSession = URLSession.shared, catch: @escaping (Swift.Error)->(), complete: @escaping ()->()) {
         request(url: url, method: .patch, body: body, catch: `catch`, complete: complete)
     }
     
-    func put<T: Encodable>(url: URL, body: T, catch: @escaping (Swift.Error)->(), complete: @escaping ()->()) {
+    public func put<T: Encodable>(url: URL,
+                           body: T,
+                           sessionManager: URLSession = URLSession.shared,
+                           catch: @escaping (Swift.Error)->(),
+                           complete: @escaping ()->()
+        ) {
         request(url: url, method: .put, body: body, catch: `catch`, complete: complete)
     }
     
     func request<T: Encodable>(url: URL,
                                method: HTTPMethod,
                                body: T,
-                               catch: @escaping (Swift.Error)->(),
+                               sessionManager: URLSession = URLSession.shared,
+                               catch throw: @escaping (Swift.Error)->(),
                                complete: @escaping ()->()
         ) {
-        let `throw` = `catch`
-        var request = URLRequest(url: url)
-        request.httpMethod = method.rawValue
         let encoder = JSONEncoder()
         do {
-            request.httpBody = try encoder.encode(body)
+            let httpBody = try encoder.encode(body)
+            request(url: url, method: method, body: httpBody, catch: `throw`, complete: complete)
         } catch let error {
             `throw`(error)
             return
         }
+    }
+
+    func request(url: URL,
+                 method: HTTPMethod,
+                 body: Data,
+                 sessionManager: URLSession = URLSession.shared,
+                 catch: @escaping (Swift.Error)->(),
+                 complete: @escaping ()->()
+        ) {
+        let `throw` = `catch`
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         task = sessionManager.dataTask(with: request) {
             (data, response, error) in
             self.task = nil
-            guard error == nil else {
-                `throw`(error!)
-                return
+            do {
+                try self.validate(response: response, error: error)
+                complete()
+            } catch let error {
+                `throw`(error)
             }
-            guard let httpResponse = response as? HTTPURLResponse else {
-                `throw`(Error.wrongResponseType)
-                return
-            }
-            guard (200..<300).contains(httpResponse.statusCode) else {
-                `throw`(Error.serverError(httpResponse.statusCode))
-                return
-            }
-            complete()
         }
         task.resume()
     }
-    func cancel() {
+
+    func decode<T: Decodable>(_ data: Data?) throws -> T {
+        guard let _data = data else {
+            throw Error.responseWasNil
+        }
+        do {
+            let decoder = JSONDecoder()
+            let responseObject: T = try decoder.decode(T.self, from: _data)
+            return  responseObject
+        } catch let error {
+            throw error
+        }
+
+    }
+    
+    func validate(response: URLResponse?, error: Swift.Error?) throws -> () {
+        guard error == nil else {
+            throw error!
+        }
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw Error.wrongResponseType
+        }
+        guard (200..<300).contains(httpResponse.statusCode) else {
+            throw Error.serverError(httpResponse.statusCode)
+        }
+    }
+    
+    public func cancel() {
         task.cancel()
         task = nil
     }
